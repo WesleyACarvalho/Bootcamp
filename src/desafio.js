a08e62bfbe836b6ce90f060bcd09ec63b53de9ea
wesley.acarvalho <wesley.talleco@gmail.com>
2023-11-09 18:02:01 -0300

    Desafio 1

---
+++ src/desafio.js
@@ -0,0 +1,31 @@
+//Nome e XP
+
+let nome = "MaveStein"
+let xp = 5000
+
+
+const texto1 = "Olá invocador, "
+const texto2 = ", seja bem vindo ao Desafio 'Bootcamp - iFood'. Sua Experiencia atual é de nivel  "
+
+if 
+(xp <= 1000) 
+console.log(texto1+nome+texto2+"Ferro")
+else if
+
+(xp <= 2000) 
+console.log(texto1+nome+texto2+"Bronze")
+else if
+(xp <= 5000) 
+console.log(texto1+nome+texto2+"Prata Ouro")
+else if
+(xp <= 8000) 
+console.log(texto1+nome+texto2+"Platina Diamante")
+else if
+(xp <= 9000) 
+console.log(texto1+nome+texto2+"Ascendente")
+else if
+(xp <= 10000) 
+console.log(texto1+nome+texto2+"Imortal")
+else 
+console.log(texto1+nome+texto2+"Radiante")
+
