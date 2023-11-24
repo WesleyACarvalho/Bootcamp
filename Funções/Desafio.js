d222bfb7e0e8878664f2a5baaf14ec93f9596792
Wesley Carvalho <137420681+WesleyACarvalho@users.noreply.github.com>
2023-11-24 16:37:37 -0300

    Desafio Felipão Concluido

---
+++ Funções/Desafio.js
@@ -0,0 +1,46 @@
+//Desafio Calculadora de partidas Rankeadas
+//O Que deve ser utilizado**
+
+//- Variáveis
+//- Operadores
+//- Laços de repetição
+//- Estruturas de decisões
+//- Funções
+
+let Vitorias = 306
+let Derrotas = 153
+let Resultado = soma(Vitorias,Derrotas)
+
+const Text1 = "O Herói tem de saldo de "
+const Text2 = "está no nível de "
+
+
+//console.log (somatorio)
+
+function
+soma(numA, numB){
+    let somatorio = numA - numB
+    return somatorio
+}
+
+if 
+(Resultado <= 10) 
+console.log ("O Herói tem de saldo de "+Resultado+" está no nível de Ferro")
+else if
+
+(Resultado <= 20) 
+console.log("O Herói tem de saldo de "+Resultado+" está no nível de Bronze")
+else if
+(Resultado <= 50) 
+console.log("O Herói tem de saldo de "+Resultado+" está no nível de Prata")
+else if
+(Resultado <= 80) 
+console.log("O Herói tem de saldo de "+Resultado+" está no nível de Ouro")
+else if
+(Resultado <= 90) 
+console.log("O Herói tem de saldo de "+Resultado+" está no nível de Diamante")
+else if
+(Resultado <= 10) 
+console.log("O Herói tem de saldo de "+Resultado+" está no nível de Lendário")
+else 
+console.log ("O Herói tem de saldo de "+Resultado+" está no nível de Imortal")
--- Funções/function_return.js
+++ Funções/function_return.js
@@ -1,7 +1,9 @@
-let Resultado = soma(5,10)
+let Derrotas = 123
+let Vitorias = 153
+let Resultado = soma(Vitorias,Derrotas)
 console.log ("Olá, sou Wesley e estou aprendendo algumas funções de JS. Neste arquivo aprendi sobre a função 'return' e o resultado foi este. 'numA' + 'numB' é: "+ Resultado)
 function
 soma(numA, numB){
-    let somatorio = numA + numB
+    let somatorio = numA - numB
     return somatorio
 }
