b74f8535f0969692c574d7bb9b019b0e275b50a6
Wesley Carvalho <137420681+WesleyACarvalho@users.noreply.github.com>
2023-11-24 16:11:30 -0300

    Final

--- Funções/function_diaadia.js
+++ Funções/function_diaadia.js
@@ -4,13 +4,14 @@ console.log("Seja bem-vindo "+userName)
 userName = getFirstName("Bianca-Acacio-Carvalho","-")
 console.log("Seja bem-vindo "+userName)
 
+// utilizado para apenas um caracter especifico
 //function getFirstName(name){
 //    let firstName = name.split(" ")[0]
 //    return firstName
 //}
 
 
-
+// Utilizado quando existe mais de um caracter de separação
 function getFirstName(name,splitChar){
     let firstName = name.split(splitChar)[0]
     return firstName
