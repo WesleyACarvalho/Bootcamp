dba50ba5fb3530696dc4029ca9fa06be5e218438
Wesley Carvalho <137420681+WesleyACarvalho@users.noreply.github.com>
2023-11-27 14:00:14 -0300

    Desafio de Classes e Objetos

---
+++ Classes e Objetos/Desafio.js
@@ -0,0 +1,23 @@
+let nome = "MaveStein"
+let idade = 26
+
+class tipo {
+    constructor (classeDoHeroi, armaDoHeroi){
+        this.classeDoHeroi = classeDoHeroi
+        this.armaDoHeroi = armaDoHeroi
+    }
+    ataque(){
+    console.log(`o ${this.classeDoHeroi} atacou usando ${this.armaDoHeroi}`)
+    }
+}
+
+let Mago = new tipo ("Mago","Magia")
+let Guerreiro = new tipo ("Guerreiro","Espada")
+let Monge = new tipo ("Monge","Artes Marciais")
+let Ninja = new tipo ("Ninja","Shuriken")
+
+
+Mago.ataque()
+Guerreiro.ataque()
+Monge.ataque()
+Ninja.ataque()
