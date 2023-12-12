6f3dc78d51d278dead8c42dd1f80fc45f9045048
WesleyACarvalho <wesley.talleco@gmail.com>
2023-12-12 17:02:12 -0300

    Finalização do JS

--- Bootcamp - PAN/JavaScript/Pokedex/assets/js/main.js
+++ Bootcamp - PAN/JavaScript/Pokedex/assets/js/main.js
@@ -1,2 +1,20 @@
 
-console.log ('Sucesso!')
+const offset = 0
+const limit = 10
+const url=`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+fetch(url)
+    .then(function(response){
+    response
+        .json()
+        .then(function(responseBody){
+            console.log(responseBody)
+    })
+
+})
+    .catch(function(error){
+        console.log(error)
+    })
+    .finally(function(){
+        console.log('Requisição concluida!')
+    })
